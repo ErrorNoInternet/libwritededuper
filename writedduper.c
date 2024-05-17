69770d5b9897dc9fcb239a859c23ce628be67244
ErrorNoInternet <errornointernet@envs.net>
2024-05-17 20:39:01 +0800

    refactor: deduplicate write functions

--- writedduper.c
+++ writedduper.c
@@ -22,6 +22,11 @@
 static int (*libc_write)(int fd, const void *buf, size_t count);
 static int (*libc_pwrite)(int fd, const void *buf, size_t count, off_t offset);
 
+enum WriteType {
+    WRITE,
+    PWRITE,
+};
+
 void __attribute__((constructor)) writedduper_init(void) {
     if ((hash_table = malloc(sizeof(HashEntry *) * pow(2, 32))) == NULL) {
         fprintf(stderr, "couldn't allocate memory: errno %d\n", errno);
@@ -44,13 +49,27 @@ void __attribute__((constructor)) writedduper_init(void) {
     };
 }
 
-ssize_t write(int fd, const void *buf, size_t count) {
-    if ((fcntl(fd, F_GETFL) & O_APPEND) == O_APPEND)
+ssize_t handle_fallback_write(enum WriteType type, int fd, const void *buf,
+                       size_t count, off_t offset) {
+    if (type == WRITE) {
         return (*libc_write)(fd, buf, count);
+    } else if (type == PWRITE) {
+        return (*libc_pwrite)(fd, buf, count, offset);
+    }
+    return 0;
+}
 
-    off_t offset = lseek(fd, 0, SEEK_CUR);
-    if (count < BLOCK_SIZE || count % BLOCK_SIZE != 0 || offset % BLOCK_SIZE != 0)
-        return (*libc_write)(fd, buf, count);
+ssize_t handle_write(enum WriteType type, int fd, const void *buf, size_t count,
+                     off_t offset) {
+    if ((fcntl(fd, F_GETFL) & O_APPEND) == O_APPEND)
+        return handle_fallback_write(type, fd, buf, count, offset);
+
+    if (type == WRITE)
+        offset = lseek(fd, 0, SEEK_CUR);
+
+    if (count < BLOCK_SIZE || count % BLOCK_SIZE != 0 ||
+        offset % BLOCK_SIZE != 0)
+        return handle_fallback_write(type, fd, buf, count, offset);
 
     char path[4096] = {0};
     char fd_link[4096];
@@ -58,16 +77,16 @@ ssize_t write(int fd, const void *buf, size_t count) {
     if (!readlink(fd_link, path, 4095)) {
         fprintf(stderr, "couldn't readlink on file descriptor %d: errno %d\n",
                 fd, errno);
-        return (*libc_write)(fd, buf, count);
+        return handle_fallback_write(type, fd, buf, count, offset);
     };
 
     HashEntry *hash_entry;
     ssize_t written, total_written = 0;
-    unsigned char s_buf[BLOCK_SIZE];
+    unsigned char block_buf[BLOCK_SIZE];
 
-    for (int s_off = 0; s_off < count; s_off += BLOCK_SIZE) {
-        memcpy(s_buf, &buf[s_off], BLOCK_SIZE);
-        uint32_t hash = calculate_crc32c(0, s_buf, BLOCK_SIZE);
+    for (int block_offset = 0; block_offset < count; block_offset += BLOCK_SIZE) {
+        memcpy(block_buf, &buf[block_offset], BLOCK_SIZE);
+        uint32_t hash = calculate_crc32c(0, block_buf, BLOCK_SIZE);
 
         if ((hash_entry = hash_table[hash]) == NULL) {
         new_block:
@@ -76,7 +95,8 @@ ssize_t write(int fd, const void *buf, size_t count) {
             hash_entry->offset = offset / BLOCK_SIZE;
             hash_table[hash] = hash_entry;
 
-            if ((written = (*libc_write)(fd, s_buf, BLOCK_SIZE)) < 0) {
+            if ((written =
+                     handle_fallback_write(type, fd, block_buf, BLOCK_SIZE, offset)) < 0) {
                 fprintf(stderr,
                         "couldn't write to file descriptor %d: errno "
                         "%d\n",
@@ -105,62 +125,10 @@ ssize_t write(int fd, const void *buf, size_t count) {
     return total_written;
 }
 
+ssize_t write(int fd, const void *buf, size_t count) {
+    return handle_write(WRITE, fd, buf, count, -1);
+}
+
 ssize_t pwrite(int fd, const void *buf, size_t count, off_t offset) {
-    if ((fcntl(fd, F_GETFL) & O_APPEND) == O_APPEND)
-        return (*libc_pwrite)(fd, buf, count, offset);
-
-    if (count < BLOCK_SIZE || count % BLOCK_SIZE != 0 || offset % BLOCK_SIZE != 0)
-        return (*libc_pwrite)(fd, buf, count, offset);
-
-    char path[4096] = {0};
-    char fd_link[4096];
-    sprintf(fd_link, "/proc/self/fd/%d", fd);
-    if (!readlink(fd_link, path, 4095)) {
-        fprintf(stderr, "couldn't readlink on file descriptor %d: errno %d\n",
-                fd, errno);
-        return (*libc_pwrite)(fd, buf, count, offset);
-    };
-
-    HashEntry *hash_entry;
-    ssize_t written, total_written = 0;
-    unsigned char s_buf[BLOCK_SIZE];
-
-    for (int s_off = 0; s_off < count; s_off += BLOCK_SIZE) {
-        memcpy(s_buf, &buf[s_off], BLOCK_SIZE);
-        uint32_t hash = calculate_crc32c(0, s_buf, BLOCK_SIZE);
-
-        if ((hash_entry = hash_table[hash]) == NULL) {
-        new_block:
-            hash_entry = malloc(sizeof(HashEntry));
-            strcpy(hash_entry->path, path);
-            hash_entry->offset = offset / BLOCK_SIZE;
-            hash_table[hash] = hash_entry;
-
-            if ((written = (*libc_pwrite)(fd, s_buf, BLOCK_SIZE, offset)) < 0) {
-                fprintf(stderr,
-                        "couldn't write to file descriptor %d: errno "
-                        "%d\n",
-                        fd, errno);
-                return -1;
-            };
-            offset += BLOCK_SIZE;
-        } else {
-            int in_fd = get_working_fd(hash_entry->path);
-            if (in_fd < 0)
-                goto new_block;
-            off_t in_position = hash_entry->offset * BLOCK_SIZE;
-
-            if ((written = copy_file_range(in_fd, &in_position, fd, &offset,
-                                           BLOCK_SIZE, 0)) < 0) {
-                fprintf(stderr,
-                        "couldn't copy_file_range on file descriptor %d: errno "
-                        "%d\n",
-                        fd, errno);
-                return -1;
-            }
-        }
-        total_written += written;
-    };
-
-    return total_written;
+    return handle_write(PWRITE, fd, buf, count, offset);
 }
