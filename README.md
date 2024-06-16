d4bd768184424da1e554240288538e62f45853e3
ErrorNoInternet <errornointernet@envs.net>
2024-06-17 02:34:51 +0800

    feat: working_fd garbage collection

--- README.md
+++ README.md
@@ -7,4 +7,3 @@ In-band deduplication via LD_PRELOAD for any filesystem that supports reflinks!
 ## To-do
 
 - Persistent on-disk hash table
-- working_fd garbage collection
--- fd.c
+++ fd.c
@@ -4,14 +4,19 @@
 #include <stdint.h>
 #include <string.h>
 #include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 
-struct hashmap *working_fds;
+#define GC_TRIGGER 1000
+#define GC_MAX_AGE 1
 
 struct WorkingFd {
     char *path;
     int fd;
+    unsigned long atime;
 };
+
+struct hashmap *working_fds;
 
 uint64_t working_fd_hash(const void *item, uint64_t seed0, uint64_t seed1) {
     const struct WorkingFd *working_fd = item;
@@ -26,16 +31,33 @@ int working_fd_compare(const void *a, const void *b, void *data) {
 }
 
 int get_working_fd(char *path) {
+    unsigned long current_time = time(NULL);
+
+    if (hashmap_count(working_fds) >= GC_TRIGGER) {
+        size_t iter = 0;
+        void *item;
+        while (hashmap_iter(working_fds, &iter, &item)) {
+            const struct WorkingFd *old_fd = item;
+            if (current_time - old_fd->atime > GC_MAX_AGE)
+                hashmap_delete(working_fds, old_fd);
+        }
+    }
+
     const struct WorkingFd *working_fd =
         hashmap_get(working_fds, &(struct WorkingFd){.path = path});
 
     if (!working_fd) {
-        int fd = open(path, O_RDWR);
-        if (fd < 0)
+        int fd;
+        if ((fd = open(path, O_RDWR)) < 0)
             return fd;
-        working_fd = &(struct WorkingFd){.path = path, .fd = fd};
-        hashmap_set(working_fds, working_fd);
+        struct WorkingFd *new_working_fd =
+            &(struct WorkingFd){.path = path, .fd = fd, .atime = current_time};
+        hashmap_set(working_fds, new_working_fd);
+        return fd;
     }
 
+    struct WorkingFd *new_working_fd = &(struct WorkingFd){
+        .path = working_fd->path, .fd = working_fd->fd, .atime = current_time};
+    hashmap_set(working_fds, working_fd);
     return working_fd->fd;
 }
